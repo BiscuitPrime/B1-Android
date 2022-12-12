f2ba4120b8acc5102d61acf465caafe2b47d2e60
BiscuitPrime <henri.nomico@gmail.com>
2022-12-12 16:10:33 +0100

    ???

--- app/src/main/java/com/droid/b1/user/UserActivity.kt
+++ app/src/main/java/com/droid/b1/user/UserActivity.kt
@@ -1,34 +1,103 @@
 package com.droid.b1.user
 
+import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import android.view.View.OnClickListener
 import android.widget.Button
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.result.launch
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.runtime.Composable
+import androidx.core.content.FileProvider
 import com.droid.b1.R
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class UserActivity : AppCompatActivity() {
 
     private var pictureButton : Button?=null;
-    private var bitmap : Bitmap?=null;
+    var image : ImageView?=null;
+
+    //variables for image :
+    var bitmap: Bitmap? = null
+    var drawable: BitmapDrawable? = null
+    var imageString = ""
+    var currentPhotoPath: String? = null
+    var photoURI: Uri? = null
+    var REQUEST_IMAGE_CAPTURE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
 
         pictureButton = findViewById(R.id.takePictureButton);
-
-        pictureButton?.setOnClickListener {
-            takePicture.launch(intent);
-        }
+        image = findViewById(R.id.imageTaken);
+        pictureButton?.setOnClickListener(
+            OnClickListener {
+                Log.d("KL0","YHBY");
+                dispatchTakePictureIntent();
+            }
+        );
 
     }
 
-    val takePicture = rememberLauncherForActivityResult(ActivityResultContracts.TakePicturePreview()) {
-        bitmap = it
+    //function that takes the picture
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    // Error occurred while creating the File
+                    Log.d("ho","FAILED");
+                    null
+                }
+                // Continue only if the File was successfully created
+                Log.d("ho","tyvqbzydh");
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "com.example.android.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            image?.setImageBitmap(imageBitmap)
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        Log.d("ho","IMG_FILE");
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
     }
 
 }
